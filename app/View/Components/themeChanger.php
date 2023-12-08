bd1df191ab19807d5cc109d0e236fc6f1cc5bd5d
Roffi Daijoubu <roffidaijoubu@gmail.com>
2023-12-08 20:16:17 +0700

    themeChanger component controller

--- app/View/Components/themeChanger.php
+++ app/View/Components/themeChanger.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class theme-changer extends Component
+class themeChanger extends Component
 {
     /**
      * Create a new component instance.
